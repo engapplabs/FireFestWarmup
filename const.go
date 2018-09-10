6ba78e2988ca81d562be71f7ae0cf87628292f25
ABuarque <abuarquemf@gmail.com>
2018-09-10 16:38:06 -0300

    const

---
+++ const.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+func main() {
+	const myConst = 20
+
+	fmt.Println(myConst)
+
+	const API_ENDPOINT = "https://api.project/v1/users"
+
+	fmt.Println(API_ENDPOINT)
+
+}
