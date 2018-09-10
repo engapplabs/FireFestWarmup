d19ee9528b6a102b834f8ace6df9c04032e994fc
ABuarque <abuarquemf@gmail.com>
2018-09-10 16:34:29 -0300

    vars

---
+++ vars.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+
+	fmt.Println(a)
+
+	var b, c = 1, 2
+
+	fmt.Println(b, c)
+
+	var boolean bool =  !false
+	fmt.Println(boolean)
+
+	arg := "short"
+	fmt.Println(arg)
+}
